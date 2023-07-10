dcddf264e61d01a9fb83b42c25d465f2373f647f
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-07-10 00:31:59 -0300

    :fire:

--- playground/app.rb
+++ playground/app.rb
@@ -1,13 +1,17 @@
-class  
+class Card
     def initialize(nome, sobreNome, idade)
         @nome = nome,
         @sobreNome = sobreNome,
         @idade = idade
     end
 
-    def setNome()
-        
+    def getCard()
+        puts "Nome: #{@nome}" or
+        puts "SobreNome: #{@sobreNome}" or
+        puts "Idade: #{@idade}"
     end
 
-
 end
+
+g = Card.new("Ian", "Peres", 18)
+g.getCard() 
