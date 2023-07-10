2be4741643e577b05e8772f730d1a386f939958c
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-07-10 00:27:24 -0300

    :fire:

---
+++ playground/app.rb
@@ -0,0 +1,13 @@
+class  
+    def initialize(nome, sobreNome, idade)
+        @nome = nome,
+        @sobreNome = sobreNome,
+        @idade = idade
+    end
+
+    def setNome()
+        
+    end
+
+
+end
