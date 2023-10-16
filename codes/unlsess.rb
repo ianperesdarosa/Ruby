55344f3a5f9a8769345250657b33a8f08f9ae68d
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-10-15 23:36:51 -0300

    commit

---
+++ codes/unlsess.rb
@@ -0,0 +1,12 @@
+day = 'Sunday'
+
+if day == 'Sunday'
+    lunch = 'Special'
+elsif day == 'no Sunday'
+    lunch = 'no Special'
+end
+unless 
+      lunch = 'Not false'
+end
+
+puts "Lunch is #{lunch} today!"
