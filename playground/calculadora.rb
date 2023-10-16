86daf3ef48890928352d3f645625aabe38906255
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-10-16 18:27:42 -0300

    commit

--- playground/calculadora.rb
+++ playground/calculadora.rb
@@ -1,9 +1,41 @@
+res = ""
+
 loop do 
-    puts "Selecione dois números!"
+    
+    puts "Selecione o primeiro número!"
     print "Digite: "
     n1 = gets.chomp.to_i
     puts "Digite o próximo número!"
     print "Digite: "
     n2 = gets.chomp.to_i
 
-    puts 
+    puts "Selecione uma das opereações: "
+    puts "1-Adição"
+    puts "2-Subtração"
+    puts "3-Multiplicação"
+    puts "4-Divisão"
+
+    print "Digite uma das opções: "
+    op = gets.chomp.to_i
+
+if op == 1
+    res = n1 + n2
+    puts "Resultado: #{res}"
+
+elsif op == 2
+    res = n1 - n2
+    puts "Resultado: #{res}"
+
+elsif op == 3 
+    res = n1 * n2
+    puts "Resultado: #{res}"
+
+elsif op == 4
+    res = n1 / n2
+    puts "Resultado: #{res}"
+
+    break
+else 
+    puts "Ocorreu um erro!"
+    end
+end
