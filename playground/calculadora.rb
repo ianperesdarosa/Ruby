3e8ee7eb1fa8984a1d67e5c092c0b01f7ce5655f
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-10-16 00:25:23 -0300

    commits

---
+++ playground/calculadora.rb
@@ -0,0 +1,9 @@
+loop do 
+    puts "Selecione dois números!"
+    print "Digite: "
+    n1 = gets.chomp.to_i
+    puts "Digite o próximo número!"
+    print "Digite: "
+    n2 = gets.chomp.to_i
+
+    puts 
