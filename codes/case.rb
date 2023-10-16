6bcae42b470696c4d8180eae52f5d36a9dba8b27
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-10-16 00:22:56 -0300

    commits

--- codes/case.rb
+++ codes/case.rb
@@ -1,6 +1,13 @@
-print ("Month: ")
-month = gets.chomp.to_i
+print ("Nota: ")
+nota = gets.chomp.to_i
 
-case month 
-when 
-    
+case nota 
+when 1..5
+    puts "ta na merda!"
+when 6..7
+    puts "Salvou raspando!"
+when 7..9
+    puts "ta suave na nave!"
+else
+    puts "Vaza gay!"
+end
