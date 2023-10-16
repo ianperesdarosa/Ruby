0ea6e8b68ef9909368d2b66ecd63778ec99cd9a6
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-10-16 20:46:31 -0300

    commit

---
+++ codes/array.rb
@@ -0,0 +1,11 @@
+/Array/
+estados = []
+/push sempre adiciona no final do array/
+estados.push("Rio Grande do Sul")
+estados.push("Santa Catarina")
+estados.push("São Paulo", "Rio de Janeiro")
+/insert adiciona elementos em qualquer posicao no array/
+estados.insert(0, "Mato Grosso do Sul")
+
+
+puts estados.count
---
+++ codes/hash.rb
@@ -0,0 +1,9 @@
+capitais = {}
+
+capitais = {acre: "Rio Branco", sao_Paulo: "Sao Paulo"}
+
+capitais[:minas_gerais] = "Belo Horizonte"
+
+capitais.keys
+
+capitais.values
