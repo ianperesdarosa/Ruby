7aa42f1e3b303433330af384457e7218a71a7084
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-10-15 23:32:21 -0300

    commit

---
+++ codes/app1.rb
@@ -0,0 +1,8 @@
+print ("Informe seu nome: ")
+name = gets.chomp
+
+print ("Informe sua idade: ")
+age = gets.chomp.to_i
+
+puts ("Nome: #{name}")
+puts ("Idade: #{age}")
---
+++ codes/if.rb
@@ -0,0 +1,11 @@
+day = 'cake'
+
+if day == 'Sunday'
+    lunch = 'Spacial'
+elsif day == "Holiday"
+    lunch = 'later'
+else
+    lunch = 'Normal'
+end
+
+puts "Lunch is #{lunch} today!"
