00cd2742ce4964b23590a72543e4a1f676d059c4
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-10-26 21:44:52 -0300

    commit

--- codes/array.rb
+++ codes/array.rb
@@ -1,10 +1,9 @@
-/Array/
+
 estados = []
-/push sempre adiciona no final do array/
+
 estados.push("Rio Grande do Sul")
 estados.push("Santa Catarina")
 estados.push("São Paulo", "Rio de Janeiro")
-/insert adiciona elementos em qualquer posicao no array/
 estados.insert(0, "Mato Grosso do Sul")
 
 
