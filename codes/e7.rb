23100e10a93b485e6c8c892e550f16034afd5ec7
Ian <ianperesdarosa2004@gmail.com>
2024-01-30 18:47:30 -0300

    commit

---
+++ codes/e7.rb
@@ -0,0 +1,9 @@
+print "Digite o número base: "
+n1 = gets.chomp.to_i
+
+print "Digite o número do expoente: "
+n2 = gets.chomp.to_i
+
+res = n1**n2
+
+puts "A potência com a base #{n1} é seu expoente #{n2} e #{res}"
