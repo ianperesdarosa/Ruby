23cabf6246dae3d4408e027553cdd44b7c8b5d8a
Ian <ianperesdarosa2004@gmail.com>
2024-01-29 21:41:10 -0300

    commit

--- codes/e5.rb
+++ codes/e5.rb
@@ -1,19 +1,19 @@
 puts "Digite as chaves e valores!"
 
 print "Digite uma chave: "
-a1 = gets.chomp.to_i.to_s
+a1 = gets.chomp
 print "Digite um valor: "
-a2 = gets.chomp.to_i.to_s
+a2 = gets.chomp
 
 print "Digite uma chave: "
-b1 = gets.chomp.to_i.to_s
+b1 = gets.chomp
 print "Digite um valor: "
-b2 = gets.chomp.to_i.to_s
+b2 = gets.chomp
 
 print "Digite uma chave: "
-c1 = gets.chomp.to_i.to_s
+c1 = gets.chomp
 print "Digite um valor: "
-c2 = gets.chomp.to_i.to_s
+c2 = gets.chomp
 
 
 db_hash = {a1=>a2, b1=>b2, c1=>c2}
