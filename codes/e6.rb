a9775f8d73d1a79359d6bb89a2f93626e2c5aa97
Ian <ianperesdarosa2004@gmail.com>
2024-01-29 23:01:15 -0300

    commit

--- codes/e6.rb
+++ codes/e6.rb
@@ -1,3 +1,16 @@
 #Gems
+require 'os'
 
+def my_os
+  if OS.windows?
+    "Windows"
+  elsif OS.linux?
+    "Linux"
+  elsif OS.mac?
+    "MacOS"
+  else
+    "Não consigo identificar o Sistema Operacional!"
+  end
+end
 
+puts "meu PC possui #{OS.cpu_count} cores, é #{OS.bits} bits e o sistema operacional é #{my_os}"
