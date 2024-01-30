f9e11d763c7eb8b31650f415385e923a940d26e6
Ian <ianperesdarosa2004@gmail.com>
2024-01-29 21:36:54 -0300

    commiy

---
+++ codes/e5.rb
@@ -0,0 +1,24 @@
+puts "Digite as chaves e valores!"
+
+print "Digite uma chave: "
+a1 = gets.chomp.to_i.to_s
+print "Digite um valor: "
+a2 = gets.chomp.to_i.to_s
+
+print "Digite uma chave: "
+b1 = gets.chomp.to_i.to_s
+print "Digite um valor: "
+b2 = gets.chomp.to_i.to_s
+
+print "Digite uma chave: "
+c1 = gets.chomp.to_i.to_s
+print "Digite um valor: "
+c2 = gets.chomp.to_i.to_s
+
+
+db_hash = {a1=>a2, b1=>b2, c1=>c2}
+
+
+db_hash.each do |k, v|
+  puts "Chave: #{k}, Valor: #{v}"
+end
